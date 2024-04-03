faa9695ccf567228c1feb88fb9436a5048b0e689
Reza Zarin <rezazarindast76@gmail.com>
2024-04-04 01:20:39 +0330

    First Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='immortal-client',
+    version='0.1',
+    packages=['immortal_client'],
+    install_requires=['requests'],
+    url='https://github.com/goldenhand76/immortal-client',
+    author='goldenhand76',
+    author_email='rezazarindast76@gmail.com',
+    description='Python client for interacting with Immortal service',
+    python_requires='>=3.6',
+)
