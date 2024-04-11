d82159bc1d497c7e26fe7c110e7c211d6d937417
Reza Zarin <rezazarindast76@gmail.com>
2024-04-11 19:57:25 +0330

    setup.py Updated

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='immortal-client',
-    version='0.1',
+    version='0.0.1',
     packages=['immortal_client'],
     install_requires=['requests'],
     url='https://github.com/goldenhand76/immortal-client',
     author='goldenhand76',
     author_email='rezazarindast76@gmail.com',
-    description='Python client for interacting with Immortals service',
+    description='Python client for interacting with Immortals',
     python_requires='>=3.6',
 )
