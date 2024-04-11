5564f282c86574ada720abaaf1639eb4959832bb
Reza Zarin <rezazarindast76@gmail.com>
2024-04-11 19:22:45 +0330

    Client get_status function has implemented

--- immortal_client/client.py
+++ immortal_client/client.py
@@ -1,12 +1,13 @@
 import requests
 
 class ImmortalClient:
-    def __init__(self, base_url):
+    def __init__(self, base_url, kafka_host):
         self.base_url = base_url
+        self.kafka_host = kafka_host
 
-    def get_status(self, address):
-        url = f"{self.base_url}/kafka-setup"
-        response = requests.post(url, json={"address": address})
+    def get_status(self):
+        url = f"{self.base_url}/kafka-host"
+        response = requests.post(url, json={"address": self.kafka_host})
         if response.status_code != 200:
             raise Exception()
         return response.json()
--- tests/get-status.py
+++ tests/get-status.py
@@ -3,15 +3,16 @@ from unittest.mock import patch
 from immortal_client.client import ImmortalClient
 
 class TestImmortalClient(unittest.TestCase):
+
     @patch('requests.get')
     def test_get_status_success(self, mock_get):
         # Arrange
-        base_url = 'http://localhost:3312'
+        base_url = 'http://localhost:8088'
         expected_status = {'status': 'running'}
         mock_get.return_value.json.return_value = expected_status
-        client = ImmortalClient(base_url)
+        client = ImmortalClient(base_url, "localhost:9092")
         # Act
-        status = client.get_status("localhost:9092")
+        status = client.get_status()
         print(status)
         # Assert
         self.assertEqual(status, expected_status)
@@ -19,13 +20,13 @@ class TestImmortalClient(unittest.TestCase):
     @patch('requests.get')
     def test_get_status_failure(self, mock_get):
         # Arrange
-        base_url = 'http://localhost:3312'
+        base_url = 'http://localhost:8088'
         mock_get.return_value.status_code = 500
-        client = ImmortalClient(base_url)
+        client = ImmortalClient(base_url, "localhost:9093")
 
         # Act & Assert
         with self.assertRaises(Exception):
-            client.get_status("localhost:9093")
+            client.get_status()
 
 if __name__ == '__main__':
     unittest.main()
